2c0fbaf1d7bdd77eb0b5658b7291e694997194a1
Fachri Maulana <fachryfachry1997@gmail.com>
2022-03-17 09:16:21 +0700

    test prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
+module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3007/",
+    publicPath:
+      argv.mode === "development"
+        ? "http://localhost:3007/"
+        : "https://store-module-federation-test-prod.vercel.app/",
   },
 
   resolve: {
@@ -63,4 +66,4 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
-};
+});
