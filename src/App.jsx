f611153159f01a059be84fb53ff1fb211c1bd89d
Fachri Maulana <fachryfachry1997@gmail.com>
2022-03-17 16:32:32 +0700

    test error prod

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import registerServiceWorker from './serviceWorkerRegistration';
 
 import "./index.scss";
 
@@ -13,4 +13,4 @@ const App = () => (
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
-serviceWorkerRegistration.register();
+registerServiceWorker();
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const Dotenv = require("dotenv-webpack");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
@@ -64,6 +66,15 @@ module.exports = (_, argv) => ({
     }),
     new HtmlWebPackPlugin({
       template: "./src/index.html",
+    }),
+    new Dotenv({
+      path: "./.env", // Path to .env file (this is the default)
+      systemvars: true,
+    }),
+
+    new InjectManifest({
+      swSrc: "./src/src-sw.js",
+      swDest: "sw.js",
     }),
   ],
 });
